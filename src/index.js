525a8cd8f93074890e5462eed69e501c0ee181fb
anafazendoanices <anasilva@Anas-Air.home>
2021-05-19 17:46:22 +0100

    Reset search function

--- src/index.js
+++ src/index.js
@@ -54,7 +54,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
-let apiKey = "ca3ccc787fe9a31582c95a99cdb93498";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "ca3ccc787fe9a31582c95a99cdb93498";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Lisbon");
