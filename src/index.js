a032e260512e05b5e8cf73a9ec04696ae5728682
anafazendoanices <anasilva@Anas-Air.home>
2021-05-20 01:04:56 +0100

    fixed search function

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
 function formatdate() {
+  let now = new Date();
+  let day = now.getDay();
+  let year = now.getFullYear();
+  let month = now.getMonth();
+  let date = now.getDate();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  day = days[now.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  month = months[now.getMonth()];
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
   let today = document.querySelector("h3");
   today.innerHTML = `${day}, ${month} ${date}, ${year} | ${hour}:${minutes}`;
 }
-
-let now = new Date();
-let day = now.getDay();
-let year = now.getFullYear();
-let month = now.getMonth();
-let date = now.getDate();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-day = days[now.getDay()];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-month = months[now.getMonth()];
-let hour = now.getHours();
-let minutes = now.getMinutes();
-
 formatdate();
 
 function displayTemperature(response) {
@@ -56,7 +54,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "ca3ccc787fe9a31582c95a99cdb93498";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
